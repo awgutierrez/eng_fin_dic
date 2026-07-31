a4eab8037a4f16fa263da17e7c8fa8c15f33a948
awgutierrez <armando.w.gutierrez.c@gmail.com>
2026-08-01 00:33:59 +0300

    update files

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@
 use std::collections::HashMap;
 
 // Implement the translate_fin function here
-fn translate_fin(dic: &HashMap<String, String>, s: &str) -> String {
+fn translate_fin(dic: &HashMap<String, String>, s: &str, usage_log: &mut HashMap<String, u32>) -> String {
+    *usage_log.entry(s.to_string()).or_insert(0) += 1;
     match dic.get(s) {
         None => return format!("sorry, no translation for {s} available"),
         Some(translation) => return format!("{} in Finnish is {}", s, translation),
@@ -24,10 +25,16 @@ fn main() {
     let dictionary: HashMap<String, String> = eng_fin_dictionary();
     println!("{dictionary:#?}");
 
-    println!("{}", translate_fin(&dictionary, "bear"));
-    println!("{}", translate_fin(&dictionary, "paw"));
-    println!("{}", translate_fin(&dictionary, "tail"));
-    println!("{}", translate_fin(&dictionary, "🐻"));
-     println!("{}", translate_fin(&dictionary, "dog")); 
+    let mut usage_log = HashMap::new();
+
+    println!("{}", translate_fin(&dictionary, "bear", &mut usage_log));
+    println!("{}", translate_fin(&dictionary, "paw", &mut usage_log));
+    println!("{}", translate_fin(&dictionary, "tail", &mut usage_log));
+    println!("{}", translate_fin(&dictionary, "bear", &mut usage_log));
+    println!("{}", translate_fin(&dictionary, "🐻", &mut usage_log));
+    println!("{}", translate_fin(&dictionary, "tail", &mut usage_log));
+    println!("{}", translate_fin(&dictionary, "🐻", &mut usage_log));
+    println!("{}", translate_fin(&dictionary, "tail", &mut usage_log));
+    println!("{usage_log:?}");
 }
 
