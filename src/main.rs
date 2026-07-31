f9f2f1ed1bab88341b40198a995418587da3b579
awgutierrez <armando.w.gutierrez.c@gmail.com>
2026-08-01 00:08:59 +0300

    update files

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 // HashMap needs to be imported from the standard library before it can be used
 use std::collections::HashMap;
 
+// Implement the translate_fin function here
+fn translate_fin(dic: &HashMap<String, String>, s: &str) -> String {
+    match dic.get(s) {
+        None => return format!("sorry, no translation for {s} available"),
+        Some(translation) => return format!("{} in Finnish is {}", s, translation),
+    };  
+}
+
 // Implement the eng_fin_dictionary function here
 fn eng_fin_dictionary() -> HashMap<String,String> {
     let dic: HashMap<String,String> = HashMap::from([
@@ -15,5 +23,11 @@ fn eng_fin_dictionary() -> HashMap<String,String> {
 fn main() {
     let dictionary: HashMap<String, String> = eng_fin_dictionary();
     println!("{dictionary:#?}");
+
+    println!("{}", translate_fin(&dictionary, "bear"));
+    println!("{}", translate_fin(&dictionary, "paw"));
+    println!("{}", translate_fin(&dictionary, "tail"));
+    println!("{}", translate_fin(&dictionary, "🐻"));
+     println!("{}", translate_fin(&dictionary, "dog")); 
 }
 
